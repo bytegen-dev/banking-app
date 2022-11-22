ca7c53c73e3ee682a12defcd4b3944f22b1078e6
tertiux44 <secondsonofadebayo@gmail.com>
2022-11-22 20:33:18 +0100

    services completed

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function notifications(){
-    // alert ('hello')
-    document.querySelector('.container').classList.toggle('run')
-    document.querySelector('.notifications').classList.toggle('blur')
-}
-
-function notif(){
-    document.querySelector('.container').classList.remove('run')
-    document.querySelector('.notifications').classList.remove('blur')
+function blurr(){
+    document.querySelector('.itemii').classList.toggle('hidde')
 }
