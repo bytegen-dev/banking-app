265b1ddc6ce7f412f16521486a30c8d8888c9623
tertiux44 <secondsonofadebayo@gmail.com>
2022-11-22 22:16:47 +0100

    recent transactions completed

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function blurr(){
     document.querySelector('.itemii').classList.toggle('hidde')
 }
+
+function pullup(){
+    document.querySelector('.transactions').classList.toggle('pull')
+    document.querySelector('.dragger').classList.toggle('pulll')
+    document.querySelector('.info').classList.toggle('brr')
+    document.querySelector('.balances').classList.toggle('brr')
+    document.querySelector('.services').classList.toggle('brr')
+    
+}
