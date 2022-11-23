70e7ccf6f7554cc4bd343160fe6762c0646db650
tertiux44 <secondsonofadebayo@gmail.com>
2022-11-23 13:55:52 +0100

    additions

--- main.js
+++ main.js
@@ -8,5 +8,29 @@ function pullup(){
     document.querySelector('.info').classList.toggle('brr')
     document.querySelector('.balances').classList.toggle('brr')
     document.querySelector('.services').classList.toggle('brr')
+    document.querySelector('.trans-container').scrollTo({
+        top:0,
+        behavior: "smooth" 
+    })
     
+}
+
+function showi(){
+    // document.querySelector('.balances').scrollTo(0, 0)
+    document.querySelector('.balances').scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+    // alert('heyy')
+}
+
+function showii(){
+    // alert('hello')
+    // document.querySelector('.balances').scrollTo(4000, 0, "smooth")
+    document.querySelector('.balances').scrollTo({
+        top: 0,
+        left: 4000,
+        behavior: "smooth"
+    })
 }
